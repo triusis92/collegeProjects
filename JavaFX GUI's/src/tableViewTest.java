06059e1e6f58044fabfdc951e4b023a197d6ea17
Lance <lancetriusis@yahoo.ie>
2019-02-20 13:22:19 +0000

    Add files via upload

---
+++ JavaFX GUI's/src/tableViewTest.java
@@ -0,0 +1,68 @@
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class tableViewTest extends Application {
+
+    Stage window;
+    TableView<tableView> table;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("thenewboston - JavaFX");
+
+        //Name column
+        TableColumn<tableView, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setMinWidth(200);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        //Price column
+        TableColumn<tableView, Double> priceColumn = new TableColumn<>("Price");
+        priceColumn.setMinWidth(100);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        //Quantity column
+        TableColumn<tableView, String> quantityColumn = new TableColumn<>("Quantity");
+        quantityColumn.setMinWidth(100);
+        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+
+        table = new TableView<>();
+        table.setItems(getProduct());
+        table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(table);
+
+        Scene scene = new Scene(vBox);
+        window.setScene(scene);
+        window.show();
+    }
+
+    //Get all of the products
+    public ObservableList<tableView> getProduct(){
+        ObservableList<tableView> products = FXCollections.observableArrayList();
+        products.add(new tableView("Laptop", 859.00, 20));
+        products.add(new tableView("Bouncy Ball", 2.49, 198));
+        products.add(new tableView("Toilet", 99.00, 74));
+        products.add(new tableView("The Notebook DVD", 19.99, 12));
+        products.add(new tableView("Corn", 1.49, 856));
+        return products;
+    }
+
+
+}
